5f00390c053b4ca6f739be581c6767c45d7a8aef
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2025-10-11 21:06:37 +0530

    operators and basics

---
+++ python/intro/datatype.py
@@ -0,0 +1,12 @@
+# int
+# float
+# string 
+# bool
+
+print(type(10))       #=> Integer
+print(type(True))     #=> Boolean
+
+
+print('ba',end = '#')
+print("hello")
+print('b','t',sep='$')
---
+++ python/intro/operator.py
@@ -0,0 +1,77 @@
+#Arithmatic operator
+
+
+#ADDITION
+print("ADDITION")
+print(2+3)
+print(2+3.4)
+print(True+False)
+print("Shivam "+"Varshney")
+print("---------\n")
+
+#Substraction
+print("Substraction")
+print(2-3)
+print(2-3.4)
+print(True-False)
+# print("Shivam "-"Varshney")           #error
+print("---------\n")
+
+#Multiplication
+print("Multiplication")
+print(2*3)
+print(2*3.4)
+print(True*False)
+# print("Shivam "*"Varshney")           #error
+print("hello "*3)
+print("---------\n")
+
+
+#Division
+print("Division")
+print(3/2)
+print(2.5/5.0)
+print(1/3)
+print("---------\n")
+
+
+#Floor division
+print("Floor division")
+print(3//2)
+print("---------\n")
+
+
+
+#Remainder
+print("Remainder")
+print(3%2)
+print(2.5%5.0)
+print(1%3)
+print("---------\n")
+
+
+#Exponentiation
+print("Exponentiation")
+print(3**2)
+print(2.5**5.0)
+print(2.5**1)
+print("---------\n")
+
+
+#abs
+print("abs")
+print(abs(-35))
+print("---------\n")
+
+
+
+#round
+print("round")
+print(round(1.2383434,2))
+print("---------\n")
+
+
+#Comparision operator <, >, <=, >=, ==, !=
+
+
+#Logical operator and(&), or , not
