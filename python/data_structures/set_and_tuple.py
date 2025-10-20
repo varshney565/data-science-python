e82d8435d2f1cef05780ba056c82e99c102d3aac
Shivam Varshney <uietcse1923shivamvarshney@kuk.ac.in>
2025-10-20 22:34:55 +0530

    tuple

--- python/data_structures/set_and_tuple.py
+++ python/data_structures/set_and_tuple.py
@@ -48,3 +48,69 @@ print("intersection:", set_1 & set_2)
 print("difference:", set_1 - set_2)
 print("symmetric difference:", set_1 ^ set_2)
 print("*"*50)
+
+
+#clearing
+my_set.clear()
+print("After clearing:",my_set)
+print("*"*50)
+
+
+#tuple data structure
+print("Tuple data structure---------------------------------")
+my_tuple = (1,2,3,6)
+print("Original tuple:",my_tuple)
+print("Length of tuple:",len(my_tuple))
+print("Accessing element at index 2:",my_tuple[2])
+print("Slicing tuple from index 1 to 3:",my_tuple[1:3])
+print("*"*50)
+
+
+#concatenation
+tuple_1 = (1,2,3)
+tuple_2 = (4,5,6)
+concatenated_tuple = tuple_1 + tuple_2
+print("Concatenated tuple:",concatenated_tuple)
+print("*"*50)
+
+
+#iterating through tuple
+print("Iterating through tuple:")
+for elem in concatenated_tuple:
+    print(elem,end=" ")
+print()
+print("*"*50)
+
+#checking presence of element
+print("Checking presence of element:")
+print(5 in concatenated_tuple)
+print(10 in concatenated_tuple)
+print("*"*50)
+
+
+#checking the count of an element
+my_tuple = (1,2,3,2,4,2)
+count_2 = my_tuple.count(2)
+print("Count of 2 in tuple:",count_2)
+print("*"*50)
+
+
+#index of an element
+index_3 = my_tuple.index(3)        #will raise ValueError if element not found
+print("Index of 3 in tuple:",index_3)
+print("*"*50)
+
+
+#multiply tuple
+my_tuple = (1,2,3)
+multiplied_tuple = my_tuple * 3
+print("Multiplied tuple:",multiplied_tuple)
+print("*"*50)
+
+#mutating a tuple by converting to list and back
+my_tuple = (1,2,3)
+temp_list = list(my_tuple)
+temp_list.append(4)
+my_tuple = tuple(temp_list)
+print("Tuple after mutation:",my_tuple)
+print("*"*50)
